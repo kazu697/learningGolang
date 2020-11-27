1e7810fc3efb9ae47174e92ed0a5716299125d29
kazu697 <kazuy0697@gmail.com>
2020-11-27 23:08:50 +0900

    ポインタの表示方法

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// 変数の宣言方法
+	i := 9
+	// ポインタの宣言方法
+	pointer := &i
 	fmt.Println("Hello World!")
+	fmt.Printf("変数の表示： %d\n", i)
+	fmt.Printf("アドレスの表示： %p\n", pointer)
+	fmt.Printf("ポインタが指している値： %v\n", *pointer)
 }
