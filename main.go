171ad69043049885b08c0773738d325875460521
kazu697 <kazuy0697@gmail.com>
2020-11-27 23:28:28 +0900

    構造体の表示

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name string
+	age int
+	height int
+}
+
 func main() {
 	// 変数の宣言方法
 	i := 9
@@ -11,4 +17,13 @@ func main() {
 	fmt.Printf("変数の表示： %d\n", i)
 	fmt.Printf("アドレスの表示： %p\n", pointer)
 	fmt.Printf("ポインタが指している値： %v\n", *pointer)
+
+	// 構造体に値を入力
+	p := Person{
+		name: "bob",
+		age: 24,
+		height: 169,
+	}
+
+	fmt.Printf("構造体の表示%#v\n", p)
 }
